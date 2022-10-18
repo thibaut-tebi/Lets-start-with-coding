5de2d39a3222e3e9d53ddae462b6d25c0c9cfcf4
TTebi <113062383+TTebi@users.noreply.github.com>
2022-10-18 07:31:40 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-name = input("Enter your real name:")
-print(name.upper())
-favourite_colour = input(" Enter favourite_colour")
-print(favourite_colour.upper())
-print("my name is " + name + " and my favourite colour is" + favourite_colour)
-date_of_birth = input("Enter your current date of birth")
-print(date_of_birth.replace("current", "actual"))
-age = 2022 - int(date_of_birth)
-print(float(age))
-if age <= 25:
-    print(" you are eligible")
-else:
-    print("not eligible")
+#the story of mr Paul
+name= " Mr. Paul"
+age= "70"
+occupation= "farming"
+print("There was a man named " + name+ ",")
+print("he was " + age + " years old.")
+print("he really like " + occupation+ ",")
+print("but did'nt like his children to go into " + occupation+".")
+
+
+#string
+
+phrase="\"Thibaut\"Academi"
+organization= " ABET"
+print(phrase + " is a very good academy")
+print(" and it is accredited by " + organization+ '.')
+print(len(phrase))
+print(phrase[15])
+
+print(phrase.index("a"))
+print(phrase.replace("Thibaut"," Jkae"))
+
+#working with numbers.
+my_number= 5.96
+print(str(my_number) + " is my highest score")
+print(round(my_number))
+ #print(sqrt(my_number))*****
+
+#sample form
+name= input("Enter your name:")
+your_age=input("Enter your age:")
+print("hello!" + name+ " you are " + your_age + " years old")
+
+#Building a basic calculator
+num1= input("Enter a number:")
+num2= input("Enter another  number:")
+result =float(num1) +float(num2)
+print(result)
+
+#MADLIBS GAME IN PYTHON
+
+#turples in pyython
+exit()

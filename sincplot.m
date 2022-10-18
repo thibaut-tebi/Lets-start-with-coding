58eb090f0c026b7565d8e0cb50e4b1d4a6b020a0
Thibaut K. Tebi <113062383+TTebi@users.noreply.github.com>
2022-10-18 09:23:06 +0000

    Add files via upload

---
+++ sincplot.m
@@ -0,0 +1,19 @@
+%id = 10908528
+%name = jeff jay
+%dept. = mten
+
+x = pi/100:pi/100:10*pi;
+y = sin(x)./x;
+
+plot(x,y,'--rs','LineWidth', 2,...
+    'Markersize',10);
+
+xlabel('\pi/100 \leg \pi/100 \leg 10\pi');
+ylabel('y=sin(x)/x)');
+
+gtext('y= sinx/x');
+title(' A graph of x against y')
+
+grid on
+
+hold off
